df3c9c721300e0012896d4ecb72ccd7366c4e861
Lotem Nadir <35609587+Lotemn102@users.noreply.github.com>
2020-09-06 21:07:32 +0300

    Update HebHTR.py

--- HebHTR.py
+++ HebHTR.py
@@ -9,37 +9,11 @@ class HebHTR:
         self.original_img = cv2.imread(img_path)
 
 
-    def imgToWords(self, remove_horziontal_lines=False, remove_vertical_lines=False, iterations=5,
-                    decoder_type='best_path'):
-        if remove_horziontal_lines:
-            clean_h = removeHorizontalLines(self.original_img)
-        else:
-            clean_h = self.original_img
-
-        if remove_vertical_lines:
-            clean_v = removeVerticalLines(clean_h)
-        else:
-            clean_v = clean_h
-
-        sentences = findSentencesBoundaries(clean_v, self.original_img)
-        words = []
-
-        if not sentences:
-            print("Couldn't find any sentences in your text, can't find words.")
-            return
-
-        for s in sentences:
-            conts = contours(s, iterations=iterations, dilate=True)
-            words.extend(cropWords(s, conts))
-
+    def imgToWord(self, iterations=5, decoder_type='best_path'):
         transcribed_words = []
         model = getModel(decoder_type=decoder_type)
-
-        for word in words:
-            transcribed_words.extend(predictWord(word, model))
-
+        transcribed_words.extend(predictWord(self.original_img, model))
         final = wordListToString(transcribed_words)
-        
         return final
 
 
