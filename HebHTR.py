dd891d9dc8dcd50206bd76320cd6214f3a2457fb
Lotem Nadir <35609587+Lotemn102@users.noreply.github.com>
2020-09-06 21:14:10 +0300

    Update HebHTR.py

--- HebHTR.py
+++ HebHTR.py
@@ -15,27 +15,3 @@ class HebHTR:
         transcribed_words.extend(predictWord(self.original_img, model))
         final = wordListToString(transcribed_words)
         return final
-
-
-    def drawRectangles(self, output_path=None, remove_horziontal_lines=False, remove_vertical_lines=False,
-                        iterations=5, dilate=True):
-
-        if remove_horziontal_lines:
-            clean_h = removeHorizontalLines(self.original_img)
-        else:
-            clean_h = self.original_img
-
-        if remove_vertical_lines:
-            clean_v = removeVerticalLines(clean_h)
-        else:
-            clean_v = clean_h
-
-        sharp_img = sharpenText(clean_v)
-        conts = contours(sharp_img, iterations, dilate)
-
-        if not output_path:
-            parent_path = os.path.dirname(self.img_path)
-            output_path = parent_path + '/draw_rectangles_result.png'
-
-        for i in conts:
-            drawRects(sharp_img, i, str(output_path))
